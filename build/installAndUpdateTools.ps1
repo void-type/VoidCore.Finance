db78c8d25dd07fc501b52699109d256eb9f61726
Jeff Schreiner <Schreinerj@prpa.org>
2019-05-13 16:07:43 -0600

    Build: dotnet format is required to build.

--- build/installAndUpdateTools.ps1
+++ build/installAndUpdateTools.ps1
@@ -7,6 +7,8 @@ dotnet tool install --global dotnet-reportgenerator-globaltool
 dotnet tool update --global dotnet-reportgenerator-globaltool
 dotnet tool install --global InheritDocTool
 dotnet tool update --global InheritDocTool
+dotnet tool install --global dotnet-format
+dotnet tool update --global dotnet-format
 
 if (-not $IncludeRecommended) {
   Exit $LASTEXITCODE
